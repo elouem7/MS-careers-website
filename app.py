e91c247ba9c0d0f91e621dc6f2ae6f8233b8f7eb
Em Elou <79684301+elouem7@users.noreply.github.com>
2023-02-24 20:06:08 +0000

    Added Home.html file, w/ Noted Body

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-  return render_template('index.html')
+def peace_Melasalem():
+    return render_template('home.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True )
